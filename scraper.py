82228a77632b2fdff1f7ea9177190af34db0e094
MekkiZ <jmekki@gmail.com>
2022-02-18 17:57:54 +0100

    new version

--- scraper.py
+++ scraper.py
@@ -9,15 +9,24 @@
 #        Import Moduls       #
 
 import logging
-import requests
-from bs4 import BeautifulSoup
-from lxml import html
 from urllib.parse import urljoin
 import csv
 import os
+import requests
+from bs4 import BeautifulSoup
+from lxml import html
+
+
 
 
 logging.basicConfig(level=logging.INFO)
+
+
+
+
+
+
+
 
 
 
@@ -31,17 +40,17 @@ def methode_scrap_pagination(url_to_scrap, folder):
         # condition to check if the Next button is available
         logging.info("        Bouton 'Next' detected AND Response: OK       ")
         try:
-            # Creat path's folder, the file CSV and Photo are there.
+        # Creat path's folder, the file CSV and Photo are there.
             os.mkdir(os.path.join(os.getcwd(), folder))
         except ValueError:
             logging.debug("Path can't creat")
 
 
-        # Creat file.
+# Creat file.
         os.chdir(os.path.join(os.getcwd(), folder))
         data_scv = open('dataScrap.csv', 'w', encoding='utf-8', newline='')
 
-        # function to creat the file CSV with columns and data.
+# function to creat the file CSV with columns and data.
         try:
             the_writer = csv.writer(data_scv, delimiter=str(';'))
             header = ['product_page_url', 'universal_ product_code', 'title', 'price_including_tax',
@@ -49,7 +58,7 @@ def methode_scrap_pagination(url_to_scrap, folder):
                       'number_available', 'product_description', 'category', 'review_rating', 'image_url']
             the_writer.writerow(header)
 
-            # Loop to navigate in pagination
+# Loop to navigate in pagination
             while True:
 
                 response = requests.get(url_to_scrap)
@@ -57,7 +66,7 @@ def methode_scrap_pagination(url_to_scrap, folder):
                 footer_element = soup.select_one('li.current')
                 logging.info(footer_element.text.strip())
 
-                # Find next page for pagination
+# Find next page for pagination
                 next_page_element = soup.select_one('li.next > a')
                 if next_page_element:
                     next_page_url = next_page_element.get('href')
@@ -67,16 +76,17 @@ def methode_scrap_pagination(url_to_scrap, folder):
                             for links in url.findAll('a', href=True):
                                 href = links.get("href")
                                 link = href.replace("../../..", "https://books.toscrape.com/catalogue")
-                                tableau = {link}
-                                for scrap in tableau:
+                                for scrap in {link}:
                                     soup = BeautifulSoup(requests.get(scrap).content, "lxml")
+                                    data_soup = soup.find_all("td")
+
 
                                     description = soup.find_all("p")[3].text
-                                    upc = soup.find_all("td")[0].text
-                                    price_exc_tax = soup.find_all("td")[2].text
-                                    price_inc_tax = soup.find_all("td")[3].text
-                                    availability = soup.find_all("td")[5].text
-                                    nb_of_rev = soup.find_all("td")[6].text
+                                    upc = data_soup[0].text
+                                    price_exc_tax = data_soup[2].text
+                                    price_inc_tax = data_soup[3].text
+                                    availability = data_soup[5].text
+                                    nb_of_rev = data_soup[6].text
                                     titres = soup.find("h1").text
                                     category = soup.find_all("a")[3].text
 
@@ -92,27 +102,30 @@ def methode_scrap_pagination(url_to_scrap, folder):
                                             finally:
                                                 photo_firsts_page.close()
 
-                                    # Data scrapped
+# Data scrapped
                                     data_scrap = [link, upc, titres, price_inc_tax, price_exc_tax, availability,
-                                                description, category, nb_of_rev, source_clean]
+                                                  description, category, nb_of_rev, source_clean]
                                     the_writer.writerow(data_scrap)
+
+# Data scrapped for the last pages
                 else:
                     for url in soup.findAll('div', class_='image_container'):
                         data = url.findAll('a', href=True)
                         for links in data:
                             href = links.get("href")
                             link = href.replace("../../..", "https://books.toscrape.com/catalogue")
-                            tableau = {link}
-                            for scrap in tableau:
+
+                            for scrap in {link}:
                                 response = requests.get(scrap)
                                 soup = BeautifulSoup(response.content, "lxml")
+                                data_soup = soup.find_all("td")
 
                                 description = soup.find_all("p")[3].text
-                                upc = soup.find_all("td")[0].text
-                                price_exc_tax = soup.find_all("td")[2].text
-                                price_inc_tax = soup.find_all("td")[3].text
-                                availability = soup.find_all("td")[5].text
-                                nb_of_rev = soup.find_all("td")[6].text
+                                upc = data_soup[0].text
+                                price_exc_tax = data_soup[2].text
+                                price_inc_tax = data_soup[3].text
+                                availability = data_soup[5].text
+                                nb_of_rev = data_soup[6].text
                                 titres = soup.find("h1").text
                                 category = soup.find_all("a")[3].text
 
@@ -127,9 +140,9 @@ def methode_scrap_pagination(url_to_scrap, folder):
                                         finally:
                                             photo_last_page.close()
 
-                                # Data scrapped
+# Data scrapped
                                 data_scrap = [link, upc, titres, price_inc_tax, price_exc_tax, availability,
-                                            description, category, nb_of_rev, source_clean]
+                                              description, category, nb_of_rev, source_clean]
                                 the_writer.writerow(data_scrap)
 
 
@@ -141,7 +154,7 @@ def methode_scrap_pagination(url_to_scrap, folder):
             data_scv.close()
     return logging.info("     Scrapping as been saved     ")
 
-
+methode_scrap_pagination("https://books.toscrape.com/catalogue/category/books/childrens_11/index.html", "chilldd")
 
 
 
@@ -176,19 +189,21 @@ def scrap_for_one_page(url_to_scrap, folder):
                 for liens in lien:
                     href = liens.get("href")
                     link_one_page = href.replace("../../..", "https://books.toscrape.com/catalogue")
-                    table = {link_one_page}
-                    for scrap in table:
+                    for scrap in {link_one_page}:
                         res = requests.get(scrap)
                         soup = BeautifulSoup(res.content, "lxml")
+                        data_soup = soup.find_all("td")
+
 
                         description = soup.find_all("p")[3].text
-                        upc = soup.find_all("td")[0].text
-                        price_exc_tax = soup.find_all("td")[2].text
-                        price_inc_tax = soup.find_all("td")[3].text
-                        availability = soup.find_all("td")[5].text
-                        nb_of_rev = soup.find_all("td")[6].text
+                        upc = data_soup[0].text
+                        price_exc_tax = data_soup[2].text
+                        price_inc_tax = data_soup[3].text
+                        availability = data_soup[5].text
+                        nb_of_rev = data_soup[6].text
                         titres = soup.find("h1").text
                         category = soup.find_all("a")[3].text
+
 
                         for div in soup.find_all('div', class_='item active'):
                             for i in div.findAll('img'):
@@ -202,10 +217,11 @@ def scrap_for_one_page(url_to_scrap, folder):
                                 finally:
                                     photos.close()
 
-                        data_one_page_scrap = [url_to_scrap, upc, titres, price_inc_tax, price_exc_tax, availability, description,
-                                    category, nb_of_rev, source_clean]
+                        data_one_page_scrap = [url_to_scrap, upc, titres, price_inc_tax, price_exc_tax, availability,
+                                               description, category, nb_of_rev, source_clean]
                         the_writer.writerow(data_one_page_scrap)
         finally:
+
             file_one_page_scrap.close()
 
 
@@ -213,17 +229,18 @@ def scrap_for_one_page(url_to_scrap, folder):
         return logging.info("END OF PROGRAM")
 
 
+#scrap_for_one_page("https://books.toscrape.com/catalogue/category/books/womens-fiction_9/index.html", "womens")
 
 
 
 def scraper(url_to_scrap: str, folder: str) -> str:
     """
-    This function scrap all important data for society.
+    This function scrap all importants data for society.
     this data is intended to help market research.
     all data will be saved in a corresponding folder.
 
     param 1 : url_to_scrap -> str
-    param 2 : folder-> str
+    param 2 : folder -> str
 
     """
     response = requests.get(url_to_scrap)
@@ -237,7 +254,6 @@ def scraper(url_to_scrap: str, folder: str) -> str:
     else:
         scrap_for_one_page(url_to_scrap, folder)
 
-    return logging.info("No problem in code")
+    logging.info("No problem in code")
 
-
-
+    return str("good job")
