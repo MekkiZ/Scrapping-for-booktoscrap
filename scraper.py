3dce7374be8803ebdd32d705f8a5f3c20a67c304
MekkiZ <jmekki@gmail.com>
2022-03-07 21:44:45 +0100

    modification final des DocString

--- scraper.py
+++ scraper.py
@@ -11,7 +11,7 @@ import requests
 def create_folder(folder: str):
     """
     Create folder containing a csv file with books details and books covers
-    :param folder: name of folder's data, entered by the user
+    :param folder: Name of folder containing data
     :return:
     """
     os.mkdir(os.path.join(os.getcwd(), folder))
@@ -23,7 +23,7 @@ def scrape_books(soup):
     """
     This function Scrape all data for each page product,
     also the function downloads all covers of books
-    :param soup: variable to invoke BeautifulSoup module
+    :param soup: BeautifulSoup instance
     """
     book_details_containers = soup.findAll("div", class_="image_container")
     for book_details_url in book_details_containers:
@@ -65,7 +65,7 @@ def scrape_books(soup):
 
 def write_to_csv(extracted_books: list):
     """
-    Write books into csv
+    Write books into csv,
     :param extracted_books: Books details scraped previously
     :return:
     """
@@ -90,9 +90,9 @@ def write_to_csv(extracted_books: list):
 def browse_and_scrape(url: str, folder: str, page_number: int = 1) -> bool:
     """
     Scrape all books from website 'books to scrape'
-    :param url of first category page
-    :param folder is the localisation for data scraped and photos
-    :param page_number: number page origin
+    :param url: Url of the first page to scrape
+    :param folder: Folder name containing scraped data and photos
+    :param page_number: Number page origin
     :return:
     """
     try:
